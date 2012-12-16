d82970f27a29fbbc56946a4dcd1fce5f4ddfc262
Brian McClain <brianmmcclain@gmail.com>
2012-12-15 20:41:33 -0500

    Parse Steam profile from XML

--- lib/steam-client/profile.rb
+++ lib/steam-client/profile.rb
@@ -1,14 +1,43 @@
+require 'crack'
+
 module SteamClient
+
+  module OnlineState
+  	ONLINE = "online",
+  	OFFLINE = "offline"
+  	UNKNOWN = "unknown"
+  end
+
   class Profile
     
-    attr_reader :steamID64, :onlineState, :avatarIcon, :avatarMedium, :avatarFull, :customURL, :hoursPlayed2Wk, :location, :realname
+    attr_accessor :steamID64, :onlineState, :avatarIcon, :avatarMedium, :avatarFull, :customURL, :hoursPlayed2Wk, :location, :realname
     
     def initalize
       
     end
     
     def self.from_xml(xml)
+      p = Crack::XML.parse(xml)
+      profile = Profile.new
       
+      profile.steamID64 = p['profile']['steamID64']
+      profile.avatarIcon = p['profile']['avatarIcon']
+      profile.avatarMedium = p['profile']['avatarMedium']
+      profile.avatarFull = p['profile']['avatarFull']
+      profile.customURL = p['profile']['customURL']
+      profile.hoursPlayed2Wk = p['profile']['hoursPlayed2Wk'].to_f
+      profile.location = p['profile']['location']
+      profile.realname = p['profile']['realname']
+
+      case p['profile']['onlineState']
+      when 'online'
+      	profile.onlineState = SteamClient::OnlineState::ONLINE
+      when 'offline'
+      	profile.onlineState = SteamClient::OnlineState::OFFLINE
+      else
+      	profile.onlineState = SteamClient::OnlineState::UNKNOWN
+      end
+      return profile
     end
   end
 end
