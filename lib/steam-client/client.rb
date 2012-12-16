f1e32041e9be21a536f6263b910bbb25b3eff462
Brian McClain <brianmmcclain@gmail.com>
2012-12-15 20:41:01 -0500

    Get profile from name

--- lib/steam-client/client.rb
+++ lib/steam-client/client.rb
@@ -1,13 +1,19 @@
+require 'rest-client'
+
 module SteamClient
   class Client
-  
+    
     attr_reader :api_key
   
-    def initialize(api_key)
+    def initialize(api_key = nil)
       @api_key = api_key
     end
 
-    
+    def find_profile_by_name(name)
+    	url = "http://steamcommunity.com/id/#{name}?xml=1"
+    	xml = RestClient.get url
+    	return Profile.from_xml(xml)
+    end
 
   end
 end
