e0257705c93f91da4106a5cdf79b41b2a9c757d6
Brian McClain <brianmmcclain@gmail.com>
2012-12-16 23:13:00 -0500

    Added retry method when receiving 503 error

--- lib/steam-client/client.rb
+++ lib/steam-client/client.rb
@@ -12,19 +12,19 @@ module SteamClient
 
     def find_profile_by_name(name)
     	url = "http://steamcommunity.com/id/#{name}?xml=1"
-    	xml = RestClient.get url
+    	xml = get_with_retries url
     	return Profile.from_xml(xml)
     end
 
     def find_profile_by_id(id)
     	url = "http://steamcommunity.com/profiles/#{id}?xml=1"
-    	xml = RestClient.get url
+    	xml = get_with_retries url
     	return Profile.from_xml(xml)
     end
 
     def get_friends_from_profile(profile)
         url = "http://steamcommunity.com/profiles/#{profile.steamID64}/friends?xml=1"
-        xml = RestClient.get url
+        xml = get_with_retries url
         hFriends = Crack::XML.parse xml
         friends = []
         hFriends['friendsList']['friends']['friend'].each do |friendID|
@@ -36,7 +36,7 @@ module SteamClient
 
     def get_games_from_profile(profile)
         url = "http://steamcommunity.com/profiles/#{profile.steamID64}/games\?xml\=1"
-        xml = RestClient.get url
+        xml = get_with_retries url
         hGames = Crack::XML.parse xml
         games = []
         hGames['gamesList']['games']['game'].each do |game|
@@ -44,6 +44,20 @@ module SteamClient
         end
         profile.games = games
         return games
+    end
+
+    def get_with_retries(url, retries = 10)
+        attempt = 0
+        while attempt < retries
+            begin
+                resp = RestClient.get url
+                return resp
+            rescue RestClient::ServiceUnavailable
+                attempt += 1
+            end
+        end
+
+        throw new RestClient::ServiceUnavailable
     end
   end
 end
